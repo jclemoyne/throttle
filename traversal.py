166d3d5adbe45da439266e19a5f06ce943060046
jclaudel <jclaudel@users.noreply.github.com>
2019-10-15 21:35:44 -0700

    re vamped from muon

--- traversal.py
+++ traversal.py
@@ -7,6 +7,7 @@ class Vertex:
 		self.class_id = None
 		self.vx_id = None
 		self.be = be
+		self.vx_id = None
 
 	def get_class_id(self, class_label):
 		cursor = self.be.cnx.cursor(buffered=True)
@@ -22,38 +23,22 @@ class Vertex:
 			for (id, ) in cursor:
 				self.class_id = id
 
+	def new(self, class_label, name):
+		self.get_class_id(class_label)
+		query_vx = (
+			"INSERT INTO vertex (class, name) VALUES ({}, \"{}\")".format(self.class_id, name))
+		cursor = self.be.cnx.cursor(buffered=True)
+		cursor.execute(query_vx)
+		self.vx_id = cursor.lastrowid
+
 
 class Traversal:
 	def __init__(self):
 		self.be = be.backend()
 
-	def addVertex(self, name, class_label):
-		cursor = self.be.cnx.cursor(buffered=True)
-		# query_vx_class = (
-		# 	"INSERT INTO vertex_class (label) "
-		# 	"VALUES (\"{}\")".format(class_label))
-		query_vx_class = (
-			"INSERT INTO vertex_class (label) "
-			"VALUES (\"{}\")".format(class_label))
-		print("\t{}".format(query_vx_class))
-		try:
-			cursor.execute(query_vx_class)
-			class_id = cursor.lastrowid
-		except mysql.connector.Error as err:
-			query_get_class_id = ("SELECT id FROM vertex_class WHERE label=\"{}\"".format(class_label))
-			print(query_get_class_id)
-			cursor.execute(query_get_class_id)
-			class_id = -1
-			for (id, ) in cursor:
-				print("~~~> {}".format(id))
-				class_id = id
-		query_vx = (
-			"INSERT INTO vertex (class, name) VALUES ({}, \"{}\")".format(class_id, name))
-		print(query_vx)
-		cursor = self.be.cnx.cursor(buffered=True)
-		cursor.execute(query_vx)
-		new_id = cursor.lastrowid
-		print("returned id: {}".format(new_id))
+	def addVertex(self, class_label, name):
+		vx = Vertex(be)
+		vx.new(class_label, name)
 
 
 if __name__ == "__main__":
